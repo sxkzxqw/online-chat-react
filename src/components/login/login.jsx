af5182ab3bd022e0ea5a11c8376d7ec0ce55a4f5
sxkzxqw <marsel.vagizov@bk.ru>
2023-04-25 19:22:10 +0300

    init

---
+++ src/components/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const login = () => {
+    return (
+        <div>
+            log
+        </div>
+    );
+};
+
+export default login;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navbar = () => {
+    return (
+        <div>
+            nav
+        </div>
+    );
+};
+
+export default navbar;
